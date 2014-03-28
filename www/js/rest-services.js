fce6321a31e8a3176942190c14f5dcdeaae2038b
Alex <acc1728@rit.edu>
2014-03-27 21:59:45 -0400

    clean up

--- www/js/rest-services.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('myApp.restServices', ['ngResource'])
-    .factory('Person', ['$resource',
-        function ($resource) {
-            return $resource('http://192.168.1.97:3000/persons/:personId', {});
-        }])
